f959fb3c2e29f5fa631e55b25fd3ac1aee3db08c
Tiffo <szuchinlin3@gmail.com>
2019-05-14 21:16:53 -0400

    11th try

--- tests/index-spec.js
+++ tests/index-spec.js
@@ -6,10 +6,12 @@ const chai = require('chai');
 
 const { expect } = chai;
 const server = require('../server');
-
+// eslint-disable-next-line no-console
 console.log('test file, server = \n', server);
 
+// eslint-disable-next-line no-undef
 describe('test', () => {
+  // eslint-disable-next-line no-undef
   it('this simple test should return a string', () => {
     expect('Test CI with Travis').to.equal('Test CI with Travis');
   });
